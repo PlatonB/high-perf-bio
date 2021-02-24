a56ca82fd349c1d7968a447aa551f4e5923ec34c
PlatonB <platon.work@gmail.com>
2021-02-24 21:34:51 +0300

    Вынос части функционала создания БД в бэкенд
    - Модуль разрешения конфликта имён БД, v1.0
    - Модуль построения объектов-индексаторов, v1.0
    
    Вынесенный из create_db в бэкенд код скоро пригодится для заливки результатов работы разных программ в новые базы (#10).

---
+++ backend/create_index_models.py
@@ -0,0 +1,34 @@
+__version__ = 'v1.0'
+
+from pymongo import IndexModel, ASCENDING
+
+def create_index_models(coll_name_ext, ind_col_names):
+        '''
+        Функция не индексирует, а лишь готовит руководящие индексацией
+        IndexModel-объекты. В случае db-VCF и db-BED, независимо от того,
+        указал исследователь имена индексируемых полей или нет, модели
+        индексации формируются для полей с локациями и основополагающими
+        идентификаторами. А то, что облигатные индексы полей с хромосами и
+        позициями проектируются составными, позволит потом парсерам эффективно
+        сортировать свои результаты стандартным для вычислительной генетики
+        образом - по геномной локации. Для обозначенных исследователем
+        полей планируется построение одиночных индексов. Если они
+        придутся на хромосому или позицию db-VCF/db-BED, то будут
+        сосуществовать с обязательным компаундным индексом. Обновить
+        набор индексов впоследствии можно будет отдельной программой из
+        состава проекта, а также с помощью MongoDB Shell или Compass.
+        '''
+        if coll_name_ext == 'vcf':
+                index_models = [IndexModel([('#CHROM', ASCENDING),
+                                            ('POS', ASCENDING)]),
+                                IndexModel([('ID', ASCENDING)])]
+        elif coll_name_ext == 'bed':
+                index_models = [IndexModel([('chrom', ASCENDING),
+                                            ('start', ASCENDING),
+                                            ('end', ASCENDING)]),
+                                IndexModel([('name', ASCENDING)])]
+        else:
+                index_models = []
+        if ind_col_names is not None:
+                index_models += [IndexModel([(ind_col_name, ASCENDING)]) for ind_col_name in ind_col_names]
+        return index_models
---
+++ backend/resolve_db_existence.py
@@ -0,0 +1,32 @@
+__version__ = 'v1.0'
+
+from pymongo import MongoClient
+
+class DbAlreadyExistsError(Exception):
+        '''
+        high-perf-bio не любит путаницу, поэтому
+        не позволяет редактировать имеющиеся БД.
+        На сегодняшний день актуально правило:
+        новый результат - в новый файл или базу.
+        '''
+        def __init__(self):
+                print('')
+                err_msg = '''\nUploading data to an existing (in particular
+source) database is not supported yet'''
+                super().__init__(err_msg)
+                
+def resolve_db_existence(db_name):
+        '''
+        Функция, дающая возможность
+        полного удаления базы данных.
+        '''
+        client = MongoClient()
+        if db_name in client.list_database_names():
+                db_to_remove = input(f'''\n{db_name} database already exists.
+To confirm database re-creation,
+type or paste its name: ''')
+                if db_to_remove == db_name:
+                        client.drop_database(db_name)
+                else:
+                        raise DbAlreadyExistsError()
+        client.close()
