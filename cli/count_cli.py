be3562b9e2c4d36e88f85af69c2979d0d91fb96e
PlatonB <platon.work@gmail.com>
2021-09-17 23:02:40 +0300

    CLI программы расчёта кол-ва значений поля, v2.0
    - Добавил англоязычную версию.
    - Улучшил некоторые формулировки.

--- cli/count_cli.py
+++ cli/count_cli.py
@@ -1,4 +1,4 @@
-__version__ = 'v1.0'
+__version__ = 'v2.0'
 
 from argparse import ArgumentParser, RawTextHelpFormatter
 
@@ -40,13 +40,62 @@ field_1.field_2.(...).field_N
         man_grp.add_argument('-T', '--trg-place', required=True, metavar='str', dest='trg_place', type=str,
                              help='Путь к папке или имя БД для результатов')
         man_grp.add_argument('-F', '--field-name', required=True, metavar='str', dest='field_name', type=str,
-                             help='Имя поля коллекций БД, для которого считать количество каждого значения')
+                             help='Имя поля, для которого считать количество каждого значения')
         opt_grp = arg_parser.add_argument_group('Необязательные аргументы')
         opt_grp.add_argument('-b', '--quan-thres', metavar='[1]', default=1, dest='quan_thres', type=int,
-                             help='Нижняя граница количества')
+                             help='Нижняя граница количества каждого значения')
         opt_grp.add_argument('-o', '--quan-sort-order', metavar='[desc]', choices=['asc', 'desc'], default='desc', dest='quan_sort_order', type=str,
-                             help='{asc, desc} Порядок сортировки по количеству')
+                             help='{asc, desc} Порядок сортировки по количеству каждого значения')
         opt_grp.add_argument('-p', '--max-proc-quan', metavar='[4]', default=4, dest='max_proc_quan', type=int,
                              help='Максимальное количество параллельно обрабатываемых коллекций')
+        args = arg_parser.parse_args()
+        return args
+
+def add_args_en(ver):
+        '''
+        Работа с аргументами командной строки.
+        '''
+        arg_parser = ArgumentParser(description=f'''
+A program that counts the quantity of each
+value of a given field within each collection.
+
+Version: {ver}
+Dependencies: MongoDB, PyMongo
+Author: Platon Bykadorov (platon.work@gmail.com), 2021
+License: GNU General Public License version 3
+Donate: https://www.tinkoff.ru/rm/bykadorov.platon1/7tX2Y99140/
+Documentation: https://github.com/PlatonB/high-perf-bio/blob/master/README-EN.md
+Bug reports, suggestions, talks: https://github.com/PlatonB/high-perf-bio/issues
+
+The counted DB must be produced by create_db.
+
+Call the nested field using a point:
+field_1.field_2.(...).field_N
+
+Due to limitations on the MongoDB side, the program does not use indexes.
+
+The notation in the CLI help:
+[default value];
+{{permissible values}}
+''',
+                                   formatter_class=RawTextHelpFormatter,
+                                   add_help=False)
+        hlp_grp = arg_parser.add_argument_group('Showing help argument')
+        hlp_grp.add_argument('-h', '--help', action='help',
+                             help='Show this help message and exit')
+        man_grp = arg_parser.add_argument_group('Mandatory arguments')
+        man_grp.add_argument('-D', '--src-db-name', required=True, metavar='str', dest='src_db_name', type=str,
+                             help='Name of the analyzed DB')
+        man_grp.add_argument('-T', '--trg-place', required=True, metavar='str', dest='trg_place', type=str,
+                             help='Path to directory or name of the DB for results')
+        man_grp.add_argument('-F', '--field-name', required=True, metavar='str', dest='field_name', type=str,
+                             help='Name of field, for which to count the quantity of each value')
+        opt_grp = arg_parser.add_argument_group('Optional arguments')
+        opt_grp.add_argument('-b', '--quan-thres', metavar='[1]', default=1, dest='quan_thres', type=int,
+                             help='Lower threshold of quantity of each value')
+        opt_grp.add_argument('-o', '--quan-sort-order', metavar='[desc]', choices=['asc', 'desc'], default='desc', dest='quan_sort_order', type=str,
+                             help='{asc, desc} Order of sorting by quantity of each value')
+        opt_grp.add_argument('-p', '--max-proc-quan', metavar='[4]', default=4, dest='max_proc_quan', type=int,
+                             help='Maximum number of collections processed in parallel')
         args = arg_parser.parse_args()
         return args
