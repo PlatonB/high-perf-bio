29bbb2cc3a70836003e4359912976b7a7117d14d
PlatonB <platon.work@gmail.com>
2021-09-22 02:48:30 +0300

    CLI программы разделения по хромосомам, v2.0
    - Добавил версию на английском.
    - Поменял пару формулировок.

--- cli/split_by_chr_cli.py
+++ cli/split_by_chr_cli.py
@@ -1,4 +1,4 @@
-__version__ = 'v1.0'
+__version__ = 'v2.0'
 
 from argparse import ArgumentParser, RawTextHelpFormatter
 
@@ -18,7 +18,7 @@ def add_args_ru(ver):
 Документация: https://github.com/PlatonB/high-perf-bio/blob/master/README.md
 Багрепорты/пожелания/общение: https://github.com/PlatonB/high-perf-bio/issues
 
-Источником разделяемых данных должна быть база, созданная с помощью create_db.
+Разделяемые данные должны находиться в базе, созданной с помощью create_db.
 
 Чтобы программа работала быстро, нужен индекс поля, в котором имена хромосом.
 
@@ -45,7 +45,7 @@ f1+f2+f3 - поля коллекций БД с составным индексо
                              help='Путь к папке или имя БД для результатов')
         opt_grp = arg_parser.add_argument_group('Необязательные аргументы')
         opt_grp.add_argument('-f', '--chrom-field-name', metavar='[None]', dest='chrom_field_name', type=str,
-                             help='Имя хромосомного поля БД (src-db-VCF, src-db-BED: не применяется, src-db-TSV: [[первое после _id поле]])')
+                             help='Имя хромосомного поля (src-db-VCF, src-db-BED: не применяется, src-db-TSV: [[первое после _id поле]])')
         opt_grp.add_argument('-k', '--proj-fields', metavar='[None]', dest='proj_fields', type=str,
                              help='Отбираемые поля (через запятую без пробела; src-db-VCF: не применяется; src-db-BED: trg-(db-)TSV; поле _id не выведется)')
         opt_grp.add_argument('-s', '--sec-delimiter', metavar='[comma]', choices=['colon', 'comma', 'low_line', 'pipe', 'semicolon'], default='comma', dest='sec_delimiter', type=str,
@@ -54,5 +54,61 @@ f1+f2+f3 - поля коллекций БД с составным индексо
                              help='Имена индексируемых полей (через запятую без пробела; trg-db-VCF: проиндексируются #CHROM+POS и ID; trg-db-BED: проиндексируются chrom+start+end и name)')
         opt_grp.add_argument('-p', '--max-proc-quan', metavar='[4]', default=4, dest='max_proc_quan', type=int,
                              help='Максимальное количество параллельно делимых коллекций')
+        args = arg_parser.parse_args()
+        return args
+
+def add_args_en(ver):
+        '''
+        Работа с аргументами командной строки.
+        '''
+        arg_parser = ArgumentParser(description=f'''
+A program that allows to split each collection
+of the MongoDB database by chromosomes.
+
+Version: {ver}
+Dependencies: MongoDB, PyMongo
+Author: Platon Bykadorov (platon.work@gmail.com), 2021
+License: GNU General Public License version 3
+Donate: https://www.tinkoff.ru/rm/bykadorov.platon1/7tX2Y99140/
+Documentation: https://github.com/PlatonB/high-perf-bio/blob/master/README-EN.md
+Bug reports, suggestions, talks: https://github.com/PlatonB/high-perf-bio/issues
+
+The divided data must be in the DB produced by create_db.
+
+For the program to process quickly, there needs index
+of the field in which are the names of the chromosomes.
+
+The notation in the CLI help:
+[default value in the argument parsing step];
+[[final default value]];
+{{permissible values}};
+scr/trg-db-FMT - source/target DB with collections,
+matching by structure to the tables in a certain format;
+trg-FMT - target tables in a certain format;
+not applicable - under the specified conditions
+the argument is ignored or causes an error;
+f1+f2+f3 - fields of the DB collections with a compound index
+''',
+                                   formatter_class=RawTextHelpFormatter,
+                                   add_help=False)
+        hlp_grp = arg_parser.add_argument_group('Showing help argument')
+        hlp_grp.add_argument('-h', '--help', action='help',
+                             help='Show this help message and exit')
+        man_grp = arg_parser.add_argument_group('Mandatory arguments')
+        man_grp.add_argument('-D', '--src-db-name', required=True, metavar='str', dest='src_db_name', type=str,
+                             help='Name of the DB whose collections to split')
+        man_grp.add_argument('-T', '--trg-place', required=True, metavar='str', dest='trg_place', type=str,
+                             help='Path to directory or name of the DB for results')
+        opt_grp = arg_parser.add_argument_group('Optional arguments')
+        opt_grp.add_argument('-f', '--chrom-field-name', metavar='[None]', dest='chrom_field_name', type=str,
+                             help='Name of the chromosomal field (src-db-VCF, src-db-BED: not applicable, src-db-TSV: [[first field after _id]])')
+        opt_grp.add_argument('-k', '--proj-fields', metavar='[None]', dest='proj_fields', type=str,
+                             help='Selectable fields (comma separated without spaces; src-db-VCF: not applicable; src-db-BED: trg-(db-)TSV; _id field will not be output)')
+        opt_grp.add_argument('-s', '--sec-delimiter', metavar='[comma]', choices=['colon', 'comma', 'low_line', 'pipe', 'semicolon'], default='comma', dest='sec_delimiter', type=str,
+                             help='{colon, comma, low_line, pipe, semicolon} Punctuation mark to restore a cell from a list (src-db-VCF, src-db-BED (trg-BED): not applicable)')
+        opt_grp.add_argument('-i', '--ind-field-names', metavar='[None]', dest='ind_field_names', type=str,
+                             help='Names of indexed fields (comma separated without spaces; trg-db-VCF: #CHROM+POS and ID will be indexed; trg-db-BED: chrom+start+end and name will be indexed)')
+        opt_grp.add_argument('-p', '--max-proc-quan', metavar='[4]', default=4, dest='max_proc_quan', type=int,
+                             help='Maximum number of collections divided in parallel')
         args = arg_parser.parse_args()
         return args
