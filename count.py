fa083a23be5bcda1b0050fd094f3349f1a883197
PlatonB <platon.work@gmail.com>
2021-09-30 23:55:15 +0300

    Программа расчёта количества значений поля, v3.0
    - Переработал алгоритм построения пайплайна для поддержки вывода и фильтрации нового поля - с частотами.
    - Исключение, если исследователь указал оба фильтра.
    - Индексация всех трёх конечных полей, если вывод - в БД.
    - Рефакторинг (#15).
    - Верхний порог количества процессов теперь не 8, а число ядер процессора.
    - Конечные файлы - .gz (#17).
    - Полный пайплайн как ещё одна метастрока.

--- count.py
+++ count.py
@@ -1,26 +1,50 @@
-__version__ = 'v2.2'
+__version__ = 'v3.0'
 
-class PrepSingleProc():
+import sys, locale, os, datetime, copy, gzip
+sys.dont_write_bytecode = True
+from cli.count_cli import add_args_ru, add_args_en
+from pymongo import MongoClient, ASCENDING, DESCENDING, IndexModel
+from backend.resolve_db_existence import resolve_db_existence, DbAlreadyExistsError
+from bson.decimal128 import Decimal128
+from bson.son import SON
+from multiprocessing import Pool
+
+class CombFiltersError(Exception):
         '''
-        Класс, спроектированный под безопасный
-        параллельный подсчёт элементарной статистики
-        для определённого поля коллекций MongoDB.
+        Считаю, что сочетание этих
+        фильтров бессмысленно и
+        лишь замедляет подсчёты.
+        Можете оспорить в Issues.
+        '''
+        def __init__(self):
+                err_msg = '\nIt is not possible to apply quantity and frequency filters at once'
+                super().__init__(err_msg)
+                
+class Main():
+        '''
+        Основной класс. args, подаваемый иниту на вход, не обязательно
+        должен формироваться argparse. Этим объектом может быть экземпляр
+        класса из стороннего Python-модуля, в т.ч. имеющего отношение к GUI.
+        Кстати, написание сообществом всевозможных графических интерфейсов
+        к high-perf-bio люто, бешено приветствуется! В ините на основе args
+        создаются как атрибуты, используемые распараллеливаемой функцией,
+        так и атрибуты, нужные для кода, её запускающего. Что касается этой
+        функции, её можно запросто пристроить в качестве коллбэка кнопки в GUI.
         '''
         def __init__(self, args, ver):
                 '''
-                Получение атрибутов, необходимых заточенной под многопроцессовое
-                выполнение функции определения количества значений заданного поля.
-                Атрибуты ни в коем случае не должны будут потом в параллельных
-                процессах изменяться. Получаются они в основном из указанных
-                исследователем аргументов. Также в ините собирается пайплайн,
-                который при выполнении способен будет не только подсчитать
-                количество каждого значения нужного поля, но и поработать
-                над этими количествами: отфильтровать, если требуется,
-                по минимальному порогу и отсортировать вверх или вниз.
+                Получение атрибутов как для основной функции программы, так и для блока
+                многопроцессового запуска таковой. Первые из перечисленных ни в коем
+                случае не должны будут потом в параллельных процессах изменяться. В
+                ините собирается пайплайн, который при выполнении способен будет не
+                только подсчитать количество и частоту каждого значения нужного поля,
+                но и поработать над этими результатами: отфильтровать, если исследователю
+                требуется, по минимальному порогу, плюс отсортировать вверх или вниз.
                 '''
                 client = MongoClient()
                 self.src_db_name = args.src_db_name
                 self.src_coll_names = client[self.src_db_name].list_collection_names()
+                self.src_coll_ext = self.src_coll_names[0].rsplit('.', maxsplit=1)[1]
                 if '/' in args.trg_place:
                         self.trg_dir_path = os.path.normpath(args.trg_place)
                 elif args.trg_place != self.src_db_name:
@@ -28,24 +52,48 @@ class PrepSingleProc():
                         resolve_db_existence(self.trg_db_name)
                 else:
                         raise DbAlreadyExistsError()
-                self.field_name = args.field_name
+                max_proc_quan = args.max_proc_quan
+                colls_quan = len(self.src_coll_names)
+                cpus_quan = os.cpu_count()
+                if max_proc_quan > colls_quan <= cpus_quan:
+                        self.proc_quan = colls_quan
+                elif max_proc_quan > cpus_quan:
+                        self.proc_quan = cpus_quan
+                else:
+                        self.proc_quan = max_proc_quan
+                if args.field_name is None:
+                        if self.src_coll_ext == 'vcf':
+                                self.field_name = 'ID'
+                        elif self.src_coll_ext == 'bed':
+                                self.field_name = 'name'
+                        else:
+                                self.field_name = 'rsID'
+                else:
+                        self.field_name = args.field_name
                 self.mongo_aggr_draft = [{'$group': {'_id': f'${self.field_name}',
-                                                     'quantity': {'$sum': 1}}}]
-                if args.quan_thres > 1:
-                        self.mongo_aggr_draft.append({'$match': {'quantity': {'$gte': args.quan_thres}}})
+                                                     'quantity': {'$sum': 1}}},
+                                         {'$addFields': {'frequency': {'$divide': ['$quantity', None]}}}]
+                self.addfields_ind = 1
+                if args.quan_thres > 1 and args.freq_thres is not None:
+                        raise CombFiltersError()
+                elif args.quan_thres > 1:
+                        self.mongo_aggr_draft.insert(1, {'$match': {'quantity': {'$gte': args.quan_thres}}})
+                        self.addfields_ind = 2
+                elif args.freq_thres is not None:
+                        self.mongo_aggr_draft.append({'$match': {'frequency': {'$gte': Decimal128(args.freq_thres)}}})
                 if args.quan_sort_order == 'asc':
-                        quan_sort_order = ASCENDING
+                        self.quan_sort_order = ASCENDING
                 elif args.quan_sort_order == 'desc':
-                        quan_sort_order = DESCENDING
-                self.mongo_aggr_draft.append({'$sort': SON([('quantity',
-                                                             quan_sort_order)])})
+                        self.quan_sort_order = DESCENDING
+                self.mongo_aggr_draft.append({'$sort': SON([('quantity', self.quan_sort_order)])})
                 self.ver = ver
                 client.close()
                 
         def count(self, src_coll_name):
                 '''
-                Функция нахождения количества
-                элементов поля одной коллекции.
+                Функция нахождения количества и
+                частоты элементов поля одной коллекции,
+                а также фильтрации полученных значений.
                 '''
                 
                 #Набор MongoDB-объектов
@@ -57,15 +105,15 @@ class PrepSingleProc():
                 src_db_obj = client[self.src_db_name]
                 src_coll_obj = src_db_obj[src_coll_name]
                 
-                #Aggregation-инструкция может быть потом дополнена
-                #индивидуальным для текущей коллекции $out-этапом.
-                #В связи с перспективой внутрипроцессовой модификации
-                #общего выражения, создаём отдельный объект с этим
-                #выражением, который точно не страшно ковырять.
+                #В связи с необходимостью внутрипроцессовой модификации общего выражения,
+                #создаём отдельный объект с этим выражением, который точно не страшно
+                #ковырять. Для получения возможности подсчёта частоты, вычисляем и
+                #внедряем в копию пайплайна количество документов всей текущей коллекции.
                 mongo_aggr_arg = copy.deepcopy(self.mongo_aggr_draft)
+                mongo_aggr_arg[self.addfields_ind]['$addFields']['frequency']['$divide'][1] = src_coll_obj.count_documents({})
                 
-                #Получаем имя конечного файла. Оно же при
-                #необходимости - имя конечной коллекции.
+                #Получаем имя конечного файла, правда, без .gz.
+                #Оно же при необходимости - имя конечной коллекции.
                 src_coll_base = src_coll_name.rsplit('.', maxsplit=1)[0]
                 trg_file_name = f'{src_coll_base}_count_res.tsv'
                 
@@ -80,12 +128,12 @@ class PrepSingleProc():
                         curs_obj = src_coll_obj.aggregate(mongo_aggr_arg,
                                                           allowDiskUse=True)
                         
-                        #Конструируем абсолютный путь к конечному файлу.
+                        #Конструируем абсолютный путь к конечному архиву.
                         trg_file_path = os.path.join(self.trg_dir_path,
-                                                     trg_file_name)
+                                                     f'{trg_file_name}.gz')
                         
                         #Открытие конечного файла на запись.
-                        with open(trg_file_path, 'w') as trg_file_opened:
+                        with gzip.open(trg_file_path, mode='wt') as trg_file_opened:
                                 
                                 #Формируем и прописываем метастроки,
                                 #повествующие о происхождении конечного
@@ -93,12 +141,13 @@ class PrepSingleProc():
                                 trg_file_opened.write(f'##tool=<{os.path.basename(__file__)[:-3]},{self.ver}>\n')
                                 trg_file_opened.write(f'##database={self.src_db_name}\n')
                                 trg_file_opened.write(f'##collection={src_coll_name}\n')
-                                trg_file_opened.write(f'{self.field_name}\tquantity\n')
+                                trg_file_opened.write(f'##pipeline={mongo_aggr_arg}\n')
+                                trg_file_opened.write(f'{self.field_name}\tquantity\tfrequency\n')
                                 
                                 #Извлечение из объекта курсора результатов, преобразование их значений
                                 #в табулированные строки и прописывание последних в конечный файл. Если
-                                #исследователь пережестил с нижним порогом количества, то кроме метастрок
-                                #в конечном файле ничего не окажется. Тогда файл будет направлен на удаление.
+                                #исследователь пережестил с порогом, то кроме метастрок в конечном
+                                #файле ничего не окажется. Тогда файл будет направлен на удаление.
                                 empty_res = True
                                 for doc in curs_obj:
                                         trg_file_opened.write('\t'.join(map(str, doc.values())) + '\n')
@@ -109,12 +158,9 @@ class PrepSingleProc():
                         if empty_res:
                                 os.remove(trg_file_path)
                                 
-                #Создание конечной базы и коллекции.
-                #Обогащение aggregation-инструкции
-                #этапом вывода в конечную коллекцию.
-                #Последняя, если не пополнилась результатами,
-                #удаляется. Для количественного поля
-                #непустой коллекции создаётся индекс.
+                #Создание конечной базы и коллекции. Обогащение aggregation-инструкции
+                #этапом вывода в конечную коллекцию. Последняя, если не пополнилась
+                #результатами, удаляется. Для полей непустой коллекции создаются индексы.
                 elif hasattr(self, 'trg_db_name'):
                         trg_db_obj = client[self.trg_db_name]
                         trg_coll_obj = trg_db_obj.create_collection(trg_file_name,
@@ -128,51 +174,28 @@ class PrepSingleProc():
                         if trg_coll_obj.count_documents({}) == 0:
                                 trg_db_obj.drop_collection(trg_file_name)
                         else:
-                                trg_coll_obj.create_index('quantity')
+                                trg_coll_obj.create_indexes([IndexModel([(self.field_name, self.quan_sort_order)]),
+                                                             IndexModel([('quantity', self.quan_sort_order)]),
+                                                             IndexModel([('frequency', self.quan_sort_order)])])
                                 
                 #Дисконнект.
                 client.close()
                 
-####################################################################################################
-
-import sys, locale, os, datetime, copy
-sys.dont_write_bytecode = True
-from cli.count_cli import add_args_ru, add_args_en
-from pymongo import MongoClient, ASCENDING, DESCENDING
-from backend.resolve_db_existence import resolve_db_existence, DbAlreadyExistsError
-from bson.son import SON
-from multiprocessing import Pool
-from backend.create_index_models import create_index_models
-
-#Подготовительный этап: обработка аргументов
-#командной строки, создание экземпляра
-#содержащего ключевую функцию класса, получение
-#имён и количества перемалываемых коллекций,
-#определение оптимального числа процессов.
-if locale.getdefaultlocale()[0][:2] == 'ru':
-        args = add_args_ru(__version__)
-else:
-        args = add_args_en(__version__)
-max_proc_quan = args.max_proc_quan
-prep_single_proc = PrepSingleProc(args,
-                                  __version__)
-src_coll_names = prep_single_proc.src_coll_names
-colls_quan = len(src_coll_names)
-if max_proc_quan > colls_quan <= 8:
-        proc_quan = colls_quan
-elif max_proc_quan > 8:
-        proc_quan = 8
-else:
-        proc_quan = max_proc_quan
-        
-print(f'\nCounting values in {prep_single_proc.src_db_name} database')
-print(f'\tnumber of parallel processes: {proc_quan}')
-
+#Обработка аргументов командной строки. Создание
+#экземпляра содержащего ключевую функцию класса.
 #Параллельный запуск расчёта. Замер времени
 #выполнения вычислений с точностью до микросекунды.
-with Pool(proc_quan) as pool_obj:
-        exec_time_start = datetime.datetime.now()
-        pool_obj.map(prep_single_proc.count, src_coll_names)
-        exec_time = datetime.datetime.now() - exec_time_start
-        
-print(f'\tparallel computation time: {exec_time}')
+if __name__ == '__main__':
+        if locale.getdefaultlocale()[0][:2] == 'ru':
+                args = add_args_ru(__version__)
+        else:
+                args = add_args_en(__version__)
+        main = Main(args, __version__)
+        proc_quan = main.proc_quan
+        print(f'\nCounting values in {main.src_db_name} database')
+        print(f'\tnumber of parallel processes: {proc_quan}')
+        with Pool(proc_quan) as pool_obj:
+                exec_time_start = datetime.datetime.now()
+                pool_obj.map(main.count, main.src_coll_names)
+                exec_time = datetime.datetime.now() - exec_time_start
+        print(f'\tparallel computation time: {exec_time}')
