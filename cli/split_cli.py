0cce927c36e6aaaec324a56758585a19c5b4e0f0
PlatonB <platon.work@gmail.com>
2021-10-31 03:52:43 +0300

    CLI программы разделения по полю, v3.0
    - Вместо аргумента деления по хромосомам теперь аргумент деления по любому из полей.
    - Убрал информацию об запрете на отбор полей src-db-VCF.
    - Максимальное количество процессов - в начало перечня необязательных аргументов.
    - Number --> quantity.

---
+++ cli/split_cli.py
@@ -0,0 +1,116 @@
+__version__ = 'v3.0'
+
+from argparse import ArgumentParser, RawTextHelpFormatter
+
+def add_args_ru(ver):
+        '''
+        Работа с аргументами командной строки.
+        '''
+        arg_parser = ArgumentParser(description=f'''
+Программа, позволяющая разбить каждую
+коллекцию MongoDB-базы по заданному полю.
+
+Версия: {ver}
+Требуемые сторонние компоненты: MongoDB, PyMongo
+Автор: Платон Быкадоров (platon.work@gmail.com), 2021
+Лицензия: GNU General Public License version 3
+Поддержать проект: https://www.tinkoff.ru/rm/bykadorov.platon1/7tX2Y99140/
+Документация: https://github.com/PlatonB/high-perf-bio/blob/master/README.md
+Багрепорты/пожелания/общение: https://github.com/PlatonB/high-perf-bio/issues
+
+Разделяемые данные должны находиться
+в базе, созданной с помощью create_db.
+
+Чтобы программа работала быстро, нужен индекс
+поля, по которому осуществляется деление.
+
+Условные обозначения в справке по CLI:
+[значение по умолчанию на этапе парсинга аргументов];
+[[конкретизированное значение по умолчанию]];
+{{допустимые значения}};
+scr/trg-db-FMT - исходная/конечная БД с коллекциями,
+соответствующими по структуре таблицам определённого формата;
+trg-FMT - конечные таблицы определённого формата;
+не применяется - при обозначенных условиях
+аргумент проигнорируется или вызовет ошибку;
+f1+f2+f3 - поля коллекций БД с составным индексом.
+''',
+                                   formatter_class=RawTextHelpFormatter,
+                                   add_help=False)
+        hlp_grp = arg_parser.add_argument_group('Аргумент вывода справки')
+        hlp_grp.add_argument('-h', '--help', action='help',
+                             help='Вывести справку и выйти')
+        man_grp = arg_parser.add_argument_group('Обязательные аргументы')
+        man_grp.add_argument('-D', '--src-db-name', required=True, metavar='str', dest='src_db_name', type=str,
+                             help='Имя БД, коллекции которой разделять')
+        man_grp.add_argument('-T', '--trg-place', required=True, metavar='str', dest='trg_place', type=str,
+                             help='Путь к папке или имя БД для результатов')
+        opt_grp = arg_parser.add_argument_group('Необязательные аргументы')
+        opt_grp.add_argument('-p', '--max-proc-quan', metavar='[4]', default=4, dest='max_proc_quan', type=int,
+                             help='Максимальное количество параллельно делимых коллекций')
+        opt_grp.add_argument('-f', '--spl-field-name', metavar='[None]', dest='spl_field_name', type=str,
+                             help='Имя поля, по которому делить (src-db-VCF: [[#CHROM]]; src-db-BED: [[chrom]]; src-db-TSV: [[первое после _id поле]])')
+        opt_grp.add_argument('-k', '--proj-fields', metavar='[None]', dest='proj_fields', type=str,
+                             help='Отбираемые поля (через запятую без пробела; src-db-VCF, src-db-BED: trg-(db-)TSV; поле _id не выведется)')
+        opt_grp.add_argument('-s', '--sec-delimiter', metavar='[comma]', choices=['colon', 'comma', 'low_line', 'pipe', 'semicolon'], default='comma', dest='sec_delimiter', type=str,
+                             help='{colon, comma, low_line, pipe, semicolon} Знак препинания для восстановления ячейки из списка (src-db-VCF, src-db-BED (trg-BED): не применяется)')
+        opt_grp.add_argument('-i', '--ind-field-names', metavar='[None]', dest='ind_field_names', type=str,
+                             help='Имена индексируемых полей (через запятую без пробела; trg-db-VCF: проиндексируются #CHROM+POS и ID; trg-db-BED: проиндексируются chrom+start+end и name)')
+        args = arg_parser.parse_args()
+        return args
+
+def add_args_en(ver):
+        '''
+        Работа с аргументами командной строки.
+        '''
+        arg_parser = ArgumentParser(description=f'''
+A program that allows to split each collection
+of the MongoDB database by specified field.
+
+Version: {ver}
+Dependencies: MongoDB, PyMongo
+Author: Platon Bykadorov (platon.work@gmail.com), 2021
+License: GNU General Public License version 3
+Donate: https://www.tinkoff.ru/rm/bykadorov.platon1/7tX2Y99140/
+Documentation: https://github.com/PlatonB/high-perf-bio/blob/master/README-EN.md
+Bug reports, suggestions, talks: https://github.com/PlatonB/high-perf-bio/issues
+
+The divided data must be in the DB produced by create_db.
+
+For the program to process quickly, there needs
+index of the field by which the splitting is done.
+
+The notation in the CLI help:
+[default value in the argument parsing step];
+[[final default value]];
+{{permissible values}};
+scr/trg-db-FMT - source/target DB with collections,
+matching by structure to the tables in a certain format;
+trg-FMT - target tables in a certain format;
+not applicable - under the specified conditions
+the argument is ignored or causes an error;
+f1+f2+f3 - fields of the DB collections with a compound index
+''',
+                                   formatter_class=RawTextHelpFormatter,
+                                   add_help=False)
+        hlp_grp = arg_parser.add_argument_group('Showing help argument')
+        hlp_grp.add_argument('-h', '--help', action='help',
+                             help='Show this help message and exit')
+        man_grp = arg_parser.add_argument_group('Mandatory arguments')
+        man_grp.add_argument('-D', '--src-db-name', required=True, metavar='str', dest='src_db_name', type=str,
+                             help='Name of the DB whose collections to split')
+        man_grp.add_argument('-T', '--trg-place', required=True, metavar='str', dest='trg_place', type=str,
+                             help='Path to directory or name of the DB for results')
+        opt_grp = arg_parser.add_argument_group('Optional arguments')
+        opt_grp.add_argument('-p', '--max-proc-quan', metavar='[4]', default=4, dest='max_proc_quan', type=int,
+                             help='Maximum quantity of collections splitted in parallel')
+        opt_grp.add_argument('-f', '--spl-field-name', metavar='[None]', dest='spl_field_name', type=str,
+                             help='Name of the field by which to split (src-db-VCF: [[#CHROM]]; src-db-BED: [[chrom]]; src-db-TSV: [[first field after _id]])')
+        opt_grp.add_argument('-k', '--proj-fields', metavar='[None]', dest='proj_fields', type=str,
+                             help='Selected fields (comma separated without spaces; src-db-VCF, src-db-BED: trg-(db-)TSV; _id field will not be output)')
+        opt_grp.add_argument('-s', '--sec-delimiter', metavar='[comma]', choices=['colon', 'comma', 'low_line', 'pipe', 'semicolon'], default='comma', dest='sec_delimiter', type=str,
+                             help='{colon, comma, low_line, pipe, semicolon} Punctuation mark to restore a cell from a list (src-db-VCF, src-db-BED (trg-BED): not applicable)')
+        opt_grp.add_argument('-i', '--ind-field-names', metavar='[None]', dest='ind_field_names', type=str,
+                             help='Names of indexed fields (comma separated without spaces; trg-db-VCF: #CHROM+POS and ID will be indexed; trg-db-BED: chrom+start+end and name will be indexed)')
+        args = arg_parser.parse_args()
+        return args
