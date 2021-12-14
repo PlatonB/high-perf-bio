01355be258a3b8bacca72ae3e47abbb293fe5188
PlatonB <platon.work@gmail.com>
2021-12-15 00:06:36 +0300

    Программа подсчёта наборов значений, v5.1
    - Отразил в именах переменных то, что у нас теперь не имена полей, а более широкое понятие - пути к полям.
    - Проверка правильности ввода путей к полям любого уровня вложенности.
    - Произвёл небольшой рефакторинг инита, в т.ч. убрал лишние self.
    - Внёс косметические изменения в код исключений.
    - Чуть улучшил читабельность имён конечных файлов.

--- count.py
+++ count.py
@@ -1,10 +1,11 @@
-__version__ = 'v5.0'
+__version__ = 'v5.1'
 
 import sys, locale, os, datetime, copy, gzip
 sys.dont_write_bytecode = True
 from cli.count_cli import add_args_ru, add_args_en
 from pymongo import MongoClient, ASCENDING, DESCENDING, IndexModel
 from backend.resolve_db_existence import resolve_db_existence, DbAlreadyExistsError
+from backend.get_field_paths import parse_nested_objs
 from bson.decimal128 import Decimal128
 from bson.son import SON
 
@@ -12,8 +13,8 @@ class MoreThanOneCollError(Exception):
         '''
         Программа работает только с одиночной коллекцией.
         '''
-        def __init__(self, src_colls_quan):
-                err_msg = f'\nThere are {src_colls_quan} collections in the DB, but it must be 1'
+        def __init__(self, colls_quan):
+                err_msg = f'\nThere are {colls_quan} collections in the DB, but it must be 1'
                 super().__init__(err_msg)
                 
 class NoSuchFieldError(Exception):
@@ -21,8 +22,8 @@ class NoSuchFieldError(Exception):
         Если исследователь, допустим, опечатавшись,
         указал поле, которого нет в коллекциях.
         '''
-        def __init__(self, field_name):
-                err_msg = f'\nThe field {field_name} does not exist'
+        def __init__(self, field_path):
+                err_msg = f'\nThe field {field_path} does not exist'
                 super().__init__(err_msg)
                 
 class CombFiltersError(Exception):
@@ -59,7 +60,8 @@ class Main():
                 '''
                 client = MongoClient()
                 self.src_db_name = args.src_db_name
-                src_coll_names = client[self.src_db_name].list_collection_names()
+                src_db_obj = client[self.src_db_name]
+                src_coll_names = src_db_obj.list_collection_names()
                 src_colls_quan = len(src_coll_names)
                 if src_colls_quan > 1:
                         raise MoreThanOneCollError(src_colls_quan)
@@ -73,42 +75,42 @@ class Main():
                 else:
                         raise DbAlreadyExistsError()
                 mongo_exclude_meta = {'meta': {'$exists': False}}
-                src_field_names = list(client[self.src_db_name][self.src_coll_name].find_one(mongo_exclude_meta))
-                if args.cnt_field_names is None:
+                src_field_paths = parse_nested_objs(src_db_obj[self.src_coll_name].find_one(mongo_exclude_meta))
+                if args.cnt_field_paths is None:
                         if src_coll_ext == 'vcf':
-                                self.cnt_field_names = ['ID', 'REF', 'ALT']
+                                cnt_field_paths = ['ID', 'REF', 'ALT']
                         elif src_coll_ext == 'bed':
-                                self.cnt_field_names = ['name']
+                                cnt_field_paths = ['name']
                         else:
-                                self.cnt_field_names = [src_field_names[1]]
+                                cnt_field_paths = [src_field_paths[1]]
                 else:
-                        self.cnt_field_names = args.cnt_field_names.split(',')
-                        for cnt_field_name in self.cnt_field_names:
-                                if cnt_field_name not in src_field_names:
-                                        raise NoSuchFieldError(cnt_field_name)
-                cnt_field_names_quan = len(self.cnt_field_names)
+                        cnt_field_paths = args.cnt_field_paths.split(',')
+                        for cnt_field_path in cnt_field_paths:
+                                if cnt_field_path not in src_field_paths:
+                                        raise NoSuchFieldError(cnt_field_path)
+                cnt_field_paths_quan = len(cnt_field_paths)
                 if args.sec_delimiter == 'colon':
-                        self.sec_delimiter = ':'
+                        sec_delimiter = ':'
                 elif args.sec_delimiter == 'comma':
-                        self.sec_delimiter = ','
+                        sec_delimiter = ','
                 elif args.sec_delimiter == 'low_line':
-                        self.sec_delimiter = '_'
+                        sec_delimiter = '_'
                 elif args.sec_delimiter == 'pipe':
-                        self.sec_delimiter = '|'
+                        sec_delimiter = '|'
                 elif args.sec_delimiter == 'semicolon':
-                        self.sec_delimiter = ';'
-                if cnt_field_names_quan > 1:
+                        sec_delimiter = ';'
+                if cnt_field_paths_quan > 1:
                         mongo_id = {'$concat': []}
-                        for cnt_field_name_ind in range(cnt_field_names_quan):
-                                mongo_id['$concat'].append({'$toString': f'${self.cnt_field_names[cnt_field_name_ind]}'})
-                                if cnt_field_name_ind < cnt_field_names_quan - 1:
-                                        mongo_id['$concat'].append(self.sec_delimiter)
+                        for cnt_field_path_ind in range(cnt_field_paths_quan):
+                                mongo_id['$concat'].append({'$toString': f'${cnt_field_paths[cnt_field_path_ind]}'})
+                                if cnt_field_path_ind < cnt_field_paths_quan - 1:
+                                        mongo_id['$concat'].append(sec_delimiter)
                 else:
-                        mongo_id = f'${self.cnt_field_names[0]}'
+                        mongo_id = f'${cnt_field_paths[0]}'
                 self.mongo_aggr_draft = [{'$match': mongo_exclude_meta},
                                          {'$group': {'_id': mongo_id,
                                                      'quantity': {'$sum': 1}}}]
-                self.header_row = [self.sec_delimiter.join(self.cnt_field_names),
+                self.trg_header = [sec_delimiter.join(cnt_field_paths),
                                    'quantity']
                 if args.quan_thres > 1 and args.freq_thres is not None:
                         raise CombFiltersError()
@@ -116,7 +118,7 @@ class Main():
                         self.mongo_aggr_draft.append({'$match': {'quantity': {'$gte': args.quan_thres}}})
                 if args.samp_quan is not None:
                         self.mongo_aggr_draft.append({'$addFields': {'frequency': {'$divide': ['$quantity', args.samp_quan]}}})
-                        self.header_row.append('frequency')
+                        self.trg_header.append('frequency')
                         if args.freq_thres is not None:
                                 self.mongo_aggr_draft.append({'$match': {'frequency': {'$gte': Decimal128(args.freq_thres)}}})
                 if args.quan_sort_order == 'asc':
@@ -147,7 +149,7 @@ class Main():
                 #Получаем имя конечного файла, правда, без .gz.
                 #Оно же при необходимости - имя конечной коллекции.
                 src_coll_base = self.src_coll_name.rsplit('.', maxsplit=1)[0]
-                trg_file_name = f'coll_{src_coll_base}__quan.tsv'
+                trg_file_name = f'coll-{src_coll_base}__quan.tsv'
                 
                 #Этот большой блок осуществляет
                 #запрос с выводом результатов в файл.
@@ -174,7 +176,7 @@ class Main():
                                 trg_file_opened.write(f'##src_db_name={self.src_db_name}\n')
                                 trg_file_opened.write(f'##src_coll_name={self.src_coll_name}\n')
                                 trg_file_opened.write(f'##mongo_aggr={mongo_aggr_arg}\n')
-                                trg_file_opened.write('\t'.join(self.header_row) + '\n')
+                                trg_file_opened.write('\t'.join(self.trg_header) + '\n')
                                 
                                 #Извлечение из объекта курсора результатов, преобразование их значений
                                 #в табулированные строки и прописывание последних в конечный файл. Если
@@ -214,7 +216,7 @@ class Main():
                         src_coll_obj.aggregate(mongo_aggr_arg, allowDiskUse=True)
                         index_models = [IndexModel([('meta', ASCENDING)])]
                         index_models += [IndexModel([(ind_field_name,
-                                                      self.quan_sort_order)]) for ind_field_name in self.header_row[1:]]
+                                                      self.quan_sort_order)]) for ind_field_name in self.trg_header[1:]]
                         trg_coll_obj.create_indexes(index_models)
                         
                 #Дисконнект.
