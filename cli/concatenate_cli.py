2c2a5c1800d961962504a39d73d2839dce8e8307
PlatonB <platon.work@gmail.com>
2022-03-07 23:13:41 +0300

    CLI программы конкатенации, v3.0
    - Заявлена поддержка создания составных индексов.
    - В хэлп-строке аргумента индексации - перечисление индексов по умолчанию, а не принудительных индексов.

--- cli/concatenate_cli.py
+++ cli/concatenate_cli.py
@@ -1,4 +1,4 @@
-__version__ = 'v2.2'
+__version__ = 'v3.0'
 
 from argparse import ArgumentParser, RawTextHelpFormatter
 
@@ -12,24 +12,24 @@ def add_args_ru(ver):
 
 Версия: {ver}
 Требуемые сторонние компоненты: MongoDB, PyMongo
-Автор: Платон Быкадоров (platon.work@gmail.com), 2021
+Автор: Платон Быкадоров (platon.work@gmail.com), 2021-2022
 Лицензия: GNU General Public License version 3
 Поддержать проект: https://www.tinkoff.ru/rm/bykadorov.platon1/7tX2Y99140/
 Документация: https://github.com/PlatonB/high-perf-bio/blob/master/README.md
 Багрепорты/пожелания/общение: https://github.com/PlatonB/high-perf-bio/issues
 
 Конкатенируемая база должна быть создана с помощью
-create_db или других инструментов high-perf-bio.
+create или других инструментов high-perf-bio.
 
 Набор полей объединяемых коллекций должен быть одинаковым.
 
 Условные обозначения в справке по CLI:
 [значение по умолчанию];
-scr/trg-db-FMT - исходная/конечная БД с коллекциями,
-соответствующими по структуре таблицам определённого формата;
+scr/trg-db-FMT - коллекции исходной/конечной БД,
+соответствующие по структуре таблицам определённого формата;
 не применяется - при обозначенных условиях
 аргумент проигнорируется или вызовет ошибку;
-f1+f2+f3 - поля коллекций БД с составным индексом.
+f1+f2+f3 - поля, для которых создавать составной индекс.
 ''',
                                     formatter_class=RawTextHelpFormatter,
                                     add_help=False)
@@ -46,8 +46,8 @@ f1+f2+f3 - поля коллекций БД с составным индексо
                              help='Отбираемые поля верхнего уровня (через запятую без пробела; src-db-VCF, src-db-BED: trg-db-TSV)')
         opt_grp.add_argument('-u', '--del-copies', dest='del_copies', action='store_true',
                              help='Удалить дубли конечных документов (-k применяется ранее; вложенные структуры не поддерживаются; _id при сравнении не учитывается)')
-        opt_grp.add_argument('-i', '--ind-field-paths', metavar='[None]', dest='ind_field_paths', type=str,
-                             help='Точечные пути к индексируемых полям (через запятую без пробела; trg-db-VCF: проиндексируются meta,#CHROM+POS,ID; trg-db-BED: <...> meta,chrom+start+end,name; trg-db-TSV: <...> meta)')
+        opt_grp.add_argument('-i', '--ind-field-groups', metavar='[None]', dest='ind_field_groups', type=str,
+                             help='Точечные пути к индексируемых полям (через запятую и/или плюс без пробела; trg-db-VCF: [[#CHROM+POS,ID]]; trg-db-BED: [[chrom+start+end]]; trg-db-TSV: [[первое после _id поле]])')
         args = arg_parser.parse_args()
         return args
 
@@ -61,23 +61,23 @@ one MongoDB database with output to another.
 
 Version: {ver}
 Dependencies: MongoDB, PyMongo
-Author: Platon Bykadorov (platon.work@gmail.com), 2021
+Author: Platon Bykadorov (platon.work@gmail.com), 2021-2022
 License: GNU General Public License version 3
 Donate: https://www.tinkoff.ru/rm/bykadorov.platon1/7tX2Y99140/
 Documentation: https://github.com/PlatonB/high-perf-bio/blob/master/README-EN.md
 Bug reports, suggestions, talks: https://github.com/PlatonB/high-perf-bio/issues
 
-Concatenated DB must be produced by create_db or other high-perf-bio tools.
+Concatenated DB must be produced by "create" or other high-perf-bio tools.
 
 The set of fields of merged collections must be the same.
 
 The notation in the CLI help:
 [default value];
-scr/trg-db-FMT - source/target DB with collections,
+scr/trg-db-FMT - collections of source/target DB,
 matching by structure to the tables in a certain format;
 not applicable - under the specified conditions
 the argument is ignored or causes an error;
-f1+f2+f3 - fields of the DB collections with a compound index
+f1+f2+f3 - fields, for which to create a compound index.
 ''',
                                     formatter_class=RawTextHelpFormatter,
                                     add_help=False)
@@ -94,7 +94,7 @@ f1+f2+f3 - fields of the DB collections with a compound index
                              help='Selected top level fields (comma separated without spaces; src-db-VCF, src-db-BED: trg-db-TSV)')
         opt_grp.add_argument('-u', '--del-copies', dest='del_copies', action='store_true',
                              help='Remove duplicates of target documents (-k is applied previously; nested structures are not supported; _id is not taken into account when comparing)')
-        opt_grp.add_argument('-i', '--ind-field-paths', metavar='[None]', dest='ind_field_paths', type=str,
-                             help='Dot paths to indexed fields (comma separated without spaces; trg-db-VCF: meta,#CHROM+POS,ID will be indexed; trg-db-BED: meta,chrom+start+end,name <...>; trg-db-TSV: meta <...>)')
+        opt_grp.add_argument('-i', '--ind-field-groups', metavar='[None]', dest='ind_field_groups', type=str,
+                             help='Dot paths to indexed fields (comma and/or plus separated without spaces; trg-db-VCF: [[#CHROM+POS,ID]]; trg-db-BED: [[chrom+start+end]]; trg-db-TSV: [[first field after _id]])')
         args = arg_parser.parse_args()
         return args
