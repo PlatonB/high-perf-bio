5773eaf95368b58a2a1ca2bd3e23f04fa5cdfc40
PlatonB <platon.work@gmail.com>
2022-03-09 22:11:29 +0300

    CLI программы разделения по полю, v5.0
    - Заявлена поддержка создания составных индексов.
    - В хэлп-строке аргумента индексации - перечисление индексов по умолчанию, а не принудительных индексов.
    - Больше нет упоминания сортировки по умолчанию в соответствующей хэлп-строке.

--- cli/split_cli.py
+++ cli/split_cli.py
@@ -1,4 +1,4 @@
-__version__ = 'v4.0'
+__version__ = 'v5.0'
 
 from argparse import ArgumentParser, RawTextHelpFormatter
 
@@ -18,8 +18,8 @@ def add_args_ru(ver):
 Документация: https://github.com/PlatonB/high-perf-bio/blob/master/README.md
 Багрепорты/пожелания/общение: https://github.com/PlatonB/high-perf-bio/issues
 
-Разделяемые данные должны находиться в базе, созданной с
-помощью create_db или других инструментов high-perf-bio.
+Разделяемые данные должны находиться в базе, созданной
+с помощью create или других инструментов high-perf-bio.
 
 Чтобы программа работала быстро, нужен индекс
 поля, по которому осуществляется деление.
@@ -31,8 +31,8 @@ def add_args_ru(ver):
 [значение по умолчанию на этапе парсинга аргументов];
 [[конкретизированное значение по умолчанию]];
 {{допустимые значения}};
-scr/trg-db-FMT - исходная/конечная БД с коллекциями,
-соответствующими по структуре таблицам определённого формата;
+scr/trg-db-FMT - коллекции исходной/конечной БД,
+соответствующие по структуре таблицам определённого формата;
 trg-FMT - конечные таблицы определённого формата;
 не применяется - при обозначенных условиях
 аргумент проигнорируется или вызовет ошибку;
@@ -55,15 +55,15 @@ f1+f2+f3 - сортируемые поля, а также поля,
         opt_grp.add_argument('-f', '--spl-field-path', metavar='[None]', dest='spl_field_path', type=str,
                              help='Точечный путь к полю, по которому делить (src-db-VCF: [[#CHROM]]; src-db-BED: [[chrom]]; src-db-TSV: [[первое после _id поле]])')
         opt_grp.add_argument('-s', '--srt-field-group', metavar='[None]', dest='srt_field_group', type=str,
-                             help='Точечные пути к сортируемым полям (через плюс без пробела; src-db-VCF: [[#CHROM+POS]]; src-db-BED: [[chrom+start+end]]; src-db-VCF, src-db-BED: trg-(db-)TSV)')
+                             help='Точечные пути к сортируемым полям (через плюс без пробела; src-db-VCF, src-db-BED: trg-(db-)TSV)')
         opt_grp.add_argument('-o', '--srt-order', metavar='[asc]', choices=['asc', 'desc'], default='asc', dest='srt_order', type=str,
                              help='{asc, desc} Порядок сортировки (применяется с -s)')
         opt_grp.add_argument('-k', '--proj-field-names', metavar='[None]', dest='proj_field_names', type=str,
                              help='Отбираемые поля верхнего уровня (через запятую без пробела; src-db-VCF, src-db-BED: trg-(db-)TSV; поле _id не выведется)')
         opt_grp.add_argument('-,', '--sec-delimiter', metavar='[comma]', choices=['colon', 'comma', 'low_line', 'pipe', 'semicolon'], default='comma', dest='sec_delimiter', type=str,
                              help='{colon, comma, low_line, pipe, semicolon} Знак препинания для восстановления ячейки из списка (src-db-VCF, src-db-BED (trg-BED): не применяется)')
-        opt_grp.add_argument('-i', '--ind-field-paths', metavar='[None]', dest='ind_field_paths', type=str,
-                             help='Точечные пути к индексируемых полям (через запятую без пробела; trg-db-VCF: проиндексируются meta,#CHROM+POS,ID; trg-db-BED: <...> meta,chrom+start+end,name; trg-db-TSV: <...> meta)')
+        opt_grp.add_argument('-i', '--ind-field-groups', metavar='[None]', dest='ind_field_groups', type=str,
+                             help='Точечные пути к индексируемых полям (через запятую и/или плюс без пробела; trg-db-VCF: [[#CHROM+POS,ID]]; trg-db-BED: [[chrom+start+end]]; trg-db-TSV: [[первое после _id поле]])')
         args = arg_parser.parse_args()
         return args
 
@@ -84,7 +84,7 @@ Documentation: https://github.com/PlatonB/high-perf-bio/blob/master/README-EN.md
 Bug reports, suggestions, talks: https://github.com/PlatonB/high-perf-bio/issues
 
 The divided data must be in the DB produced
-by "create_db" or other high-perf-bio tools.
+by "create" or other high-perf-bio tools.
 
 For the program to process quickly, there needs
 index of the field by which the splitting is done.
@@ -96,7 +96,7 @@ The notation in the CLI help:
 [default value in the argument parsing step];
 [[final default value]];
 {{permissible values}};
-scr/trg-db-FMT - source/target DB with collections,
+scr/trg-db-FMT - collections of source/target DB,
 matching by structure to the tables in a certain format;
 trg-FMT - target tables in a certain format;
 not applicable - under the specified conditions
@@ -120,14 +120,14 @@ for which to create a compound index.
         opt_grp.add_argument('-f', '--spl-field-path', metavar='[None]', dest='spl_field_path', type=str,
                              help='Dot path to the field by which to split (src-db-VCF: [[#CHROM]]; src-db-BED: [[chrom]]; src-db-TSV: [[first field after _id]])')
         opt_grp.add_argument('-s', '--srt-field-group', metavar='[None]', dest='srt_field_group', type=str,
-                             help='Dot paths to sorted fields (plus separated without spaces; src-db-VCF: [[#CHROM+POS]]; src-db-BED: [[chrom+start+end]]; src-db-VCF, src-db-BED: trg-(db-)TSV)')
+                             help='Dot paths to sorted fields (plus separated without spaces; src-db-VCF, src-db-BED: trg-(db-)TSV)')
         opt_grp.add_argument('-o', '--srt-order', metavar='[asc]', choices=['asc', 'desc'], default='asc', dest='srt_order', type=str,
                              help='{asc, desc} Order of sorting (applicable with -s)')
         opt_grp.add_argument('-k', '--proj-field-names', metavar='[None]', dest='proj_field_names', type=str,
                              help='Selected top level fields (comma separated without spaces; src-db-VCF, src-db-BED: trg-(db-)TSV; _id field will not be output)')
         opt_grp.add_argument('-,', '--sec-delimiter', metavar='[comma]', choices=['colon', 'comma', 'low_line', 'pipe', 'semicolon'], default='comma', dest='sec_delimiter', type=str,
                              help='{colon, comma, low_line, pipe, semicolon} Punctuation mark to restore a cell from a list (src-db-VCF, src-db-BED (trg-BED): not applicable)')
-        opt_grp.add_argument('-i', '--ind-field-paths', metavar='[None]', dest='ind_field_paths', type=str,
-                             help='Dot paths to indexed fields (comma separated without spaces; trg-db-VCF: meta,#CHROM+POS,ID will be indexed; trg-db-BED: meta,chrom+start+end,name <...>; trg-db-TSV: meta <...>)')
+        opt_grp.add_argument('-i', '--ind-field-groups', metavar='[None]', dest='ind_field_groups', type=str,
+                             help='Dot paths to indexed fields (comma and/or plus separated without spaces; trg-db-VCF: [[#CHROM+POS,ID]]; trg-db-BED: [[chrom+start+end]]; trg-db-TSV: [[first field after _id]])')
         args = arg_parser.parse_args()
         return args
