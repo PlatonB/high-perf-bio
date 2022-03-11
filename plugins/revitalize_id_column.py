e7da6f0b98fa9520533949095c4c47833e32d853
PlatonB <platon.work@gmail.com>
2022-03-11 21:05:12 +0300

    Программа восстановления ID-поля VCF, v3.0
    - Рефакторинг (#15).
    - Импорт одного из исключений из отдельного модуля.
    - Верхний порог количества процессов теперь не 8, а число ядер процессора.
    - Новая структура имён конечных файлов.

--- plugins/revitalize_id_column.py
+++ plugins/revitalize_id_column.py
@@ -1,13 +1,17 @@
-__version__ = 'v2.3'
+__version__ = 'v3.0'
 
-class DifFmtsError(Exception):
-        '''
-        Исходные таблицы могут быть только одноформатными.
-        '''
-        def __init__(self, src_file_fmts):
-                err_msg = f'\nSource files are in different formats: {src_file_fmts}'
-                super().__init__(err_msg)
-                
+import sys, os, locale, datetime, gzip
+sys.dont_write_bytecode = True
+sys.path.append(os.path.join(os.path.dirname(os.getcwd()),
+                             'cli'))
+sys.path.append(os.path.join(os.path.dirname(os.getcwd()),
+                             'backend'))
+from revitalize_id_column_cli import add_args_ru, add_args_en
+from pymongo import MongoClient, ASCENDING
+from common_errors import DifFmtsError
+from multiprocessing import Pool
+from def_data_type import def_data_type
+
 class NotVcfError(Exception):
         '''
         Предотвращение попытки работать с
@@ -25,21 +29,25 @@ class MoreThanOneCollError(Exception):
                 err_msg = f'\nThere are {src_colls_quan} collections in the DB, but it must be 1'
                 super().__init__(err_msg)
                 
-class PrepSingleProc():
+class Main():
         '''
-        Класс, спроектированный под безопасное
-        параллельное восстановление набора VCF.
+        Основной класс. args, подаваемый иниту на вход, не обязательно
+        должен формироваться argparse. Этим объектом может быть экземпляр
+        класса из стороннего Python-модуля, в т.ч. имеющего отношение к GUI.
+        Кстати, написание сообществом всевозможных графических интерфейсов
+        к high-perf-bio люто, бешено приветствуется! В ините на основе args
+        создаются как атрибуты, используемые распараллеливаемой функцией,
+        так и атрибуты, нужные для кода, её запускающего. Что касается этой
+        функции, её можно запросто пристроить в качестве коллбэка кнопки в GUI.
         '''
-        def __init__(self, args, ver):
+        def __init__(self, args):
                 '''
-                Получение атрибутов, необходимых заточенной под
-                многопроцессовое выполнение функции наводнения
-                ID-столбца rs-идентификаторами. Атрибуты ни в
-                коем случае не должны будут потом в параллельных
-                процессах изменяться. Получаются они в основном
-                из указанных исследователем аргументов. Здесь же -
-                в инициализирующем методе - выставлены несколько
-                ограничений, обусловленных узкой специализицией
+                Получение атрибутов как для основной функции программы,
+                так и для блока многопроцессового запуска таковой.
+                Первые из перечисленных ни в коем случае не должны
+                будут потом в параллельных процессах изменяться.
+                Здесь же - в инициализирующем методе - выставлены
+                несколько ограничений, обусловленных узкой специализицией
                 программы. К примеру, произойдёт аварийное завершение,
                 если на вход будет подан файл в не-VCF формате.
                 '''
@@ -63,108 +71,91 @@ class PrepSingleProc():
                 if src_coll_ext != 'vcf':
                         raise NotVcfError(src_coll_ext)
                 self.trg_dir_path = os.path.normpath(args.trg_dir_path)
+                max_proc_quan = args.max_proc_quan
+                src_files_quan = len(self.src_file_names)
+                cpus_quan = os.cpu_count()
+                if max_proc_quan > src_files_quan <= cpus_quan:
+                        self.proc_quan = src_files_quan
+                elif max_proc_quan > cpus_quan:
+                        self.proc_quan = cpus_quan
+                else:
+                        self.proc_quan = max_proc_quan
                 self.ignore_unrev_lines = args.ignore_unrev_lines
                 client.close()
                 
         def revitalize(self, src_file_name):
                 '''
-                Функция заделывания столбца
-                ID rs-идентификаторами.
+                Функция заделывания столбца ID rs-идентификаторами.
+                Расскажу о неочевидных моментах. Набор MongoDB-объектов
+                должен быть строго индивидуальным для каждого процесса,
+                иначе возможны конфликты. Дальше речь пойдёт о, собственно,
+                получении и верификации rsIDs. Поиск MongoDB-документа с
+                rs-идентификатором делается по хромосоме, позиции и всем
+                аллелям. Вариант как из src-VCF, так и из src-db-VCF, может
+                оказаться мультиаллельным. В одних VCF альтернативные аллели
+                перечислены через запятую, в других - разнесены по разным
+                строкам, что абсолютно легально. src-db-VCF-эквивалент
+                записи через запятую - массив (список). О подготовке этих
+                списков заботится отвественный за создание БД компонент
+                high-perf-bio. Поиск обособленного альтернативного аллеля
+                по ALT-полю коллекции не требует каких-то плясок с бубном:
+                MongoDB автоматически чекает вхождение запрашиваемого
+                слова в каждый существующий в поле список. Если ALT-аллели
+                src-VCF оформлены через запятую, то программа их дробит
+                на списки и задействует функциональность MongoDB искать
+                точное соответствие целых списков. Сочетание неразбитых
+                ALT src-VCF с разбитыми в src-db-VCF не поддерживается,
+                но и вряд ли встретится в реальной жизни.
                 '''
-                
-                #Набор MongoDB-объектов
-                #должен быть строго
-                #индивидуальным для
-                #каждого процесса, иначе
-                #возможны конфликты.
                 client = MongoClient()
                 src_coll_obj = client[self.src_db_name][self.src_coll_name]
-                
-                #Поиск MongoDB-документа с rs-идентификатором делается по хромосоме, позиции и всем аллелям.
-                #Вариант как из src-VCF, так и из src-db-VCF, может оказаться мультиаллельным. В одних VCF
-                #альтернативные аллели перечислены через запятую, в других - разнесены по разным строкам, что
-                #абсолютно легально. src-db-VCF-эквивалент записи через запятую - список. О подготовке этих
-                #списков заботится отвественный за создание БД компонент high-perf-bio. Поиск обособленного
-                #альтернативного аллеля по ALT-полю коллекции не требует каких-то плясок с бубном: MongoDB
-                #автоматически чекает вхождение запрашиваемого слова в каждый существующий в поле список. Если
-                #ALT-аллели src-VCF оформлены через запятую, то программа их дробит на списки и задействует
-                #функциональность MongoDB искать точное соответствие целых списков. Сочетание неразбитых ALT
-                #src-VCF с разбитыми в src-db-VCF не поддерживается, но и вряд ли встретится в реальной жизни.
+                src_file_base = src_file_name.rsplit('.', maxsplit=2)[0]
+                src_coll_base = self.src_coll_name.rsplit('.', maxsplit=1)[0]
+                trg_file_name = f'file-{src_file_base}__coll-{src_coll_base}.vcf.gz'
                 with gzip.open(os.path.join(self.src_dir_path, src_file_name), mode='rt') as src_file_opened:
-                        src_file_base = src_file_name.rsplit('.', maxsplit=2)[0]
-                        trg_file_name = f'{src_file_base}_rsIDs.vcf.gz'
-                        trg_file_path = os.path.join(self.trg_dir_path, trg_file_name)
-                        with gzip.open(trg_file_path, mode='wt') as trg_file_opened:
-                                for line in src_file_opened:
-                                        trg_file_opened.write(line)
-                                        if not line.startswith('##'):
+                        with gzip.open(os.path.join(self.trg_dir_path, trg_file_name), mode='wt') as trg_file_opened:
+                                for src_line in src_file_opened:
+                                        trg_file_opened.write(src_line)
+                                        if not src_line.startswith('##'):
                                                 break
-                                for line in src_file_opened:
-                                        row = line.split('\t')
-                                        chrom = def_data_type(row[0].replace('chr', ''))
-                                        pos = int(row[1])
-                                        ref = row[3]
-                                        if ',' not in row[4]:
-                                                alt = row[4]
+                                for src_line in src_file_opened:
+                                        src_row = src_line.split('\t')
+                                        src_chrom = def_data_type(src_row[0].replace('chr', ''))
+                                        src_pos = int(src_row[1])
+                                        src_ref = src_row[3]
+                                        if ',' not in src_row[4]:
+                                                src_alt = src_row[4]
                                         else:
-                                                alt = row[4].split(',')
-                                        doc = src_coll_obj.find_one({'#CHROM': chrom,
-                                                                     'POS': pos,
-                                                                     'REF': ref,
-                                                                     'ALT': alt})
+                                                src_alt = src_row[4].split(',')
+                                        doc = src_coll_obj.find_one({'#CHROM': src_chrom,
+                                                                     'POS': src_pos,
+                                                                     'REF': src_ref,
+                                                                     'ALT': src_alt})
                                         if doc is not None:
-                                                row[2] = doc['ID']
-                                                trg_file_opened.write('\t'.join(row))
+                                                src_row[2] = doc['ID']
+                                                trg_file_opened.write('\t'.join(src_row))
                                         elif not self.ignore_unrev_lines:
-                                                trg_file_opened.write(line)
+                                                trg_file_opened.write(src_line)
                                                 
                 #Дисконнект.
                 client.close()
                 
-####################################################################################################
-
-import sys, locale, os, datetime, gzip
-sys.dont_write_bytecode = True
-sys.path.append(os.path.join(os.path.dirname(os.getcwd()),
-                             'cli'))
-sys.path.append(os.path.join(os.path.dirname(os.getcwd()),
-                             'backend'))
-from revitalize_id_column_cli import add_args_ru, add_args_en
-from pymongo import MongoClient, ASCENDING
-from multiprocessing import Pool
-from def_data_type import def_data_type
-
-#Подготовительный этап: обработка
-#аргументов командной строки,
-#создание экземпляра содержащего
-#ключевую функцию класса,
-#получение имён и количества
-#реставрируемых файлов, определение
-#оптимального числа процессов.
-if locale.getdefaultlocale()[0][:2] == 'ru':
-        args = add_args_ru(__version__)
-else:
-        args = add_args_en(__version__)
-max_proc_quan = args.max_proc_quan
-prep_single_proc = PrepSingleProc(args,
-                                  __version__)
-src_file_names = prep_single_proc.src_file_names
-src_files_quan = len(src_file_names)
-if max_proc_quan > src_files_quan <= 8:
-        proc_quan = src_files_quan
-elif max_proc_quan > 8:
-        proc_quan = 8
-else:
-        proc_quan = max_proc_quan
-        
-print(f'\nID column reconstruction by {prep_single_proc.src_db_name} database')
-print(f'\tnumber of parallel processes: {proc_quan}')
-
-#Параллельный запуск реабилитации ID. Замер времени
-#выполнения этого кода с точностью до микросекунды.
-with Pool(proc_quan) as pool_obj:
-        exec_time_start = datetime.datetime.now()
-        pool_obj.map(prep_single_proc.revitalize, src_file_names)
-        exec_time = datetime.datetime.now() - exec_time_start
-        
-print(f'\tparallel computation time: {exec_time}')
+#Обработка аргументов командной строки.
+#Создание экземпляра содержащего ключевую
+#функцию класса. Параллельный запуск
+#реабилитации ID. Замер времени выполнения
+#вычислений с точностью до микросекунды.
+if __name__ == '__main__':
+        if locale.getdefaultlocale()[0][:2] == 'ru':
+                args = add_args_ru(__version__)
+        else:
+                args = add_args_en(__version__)
+        main = Main(args)
+        proc_quan = main.proc_quan
+        print(f'\nID column reconstruction by {main.src_db_name} DB')
+        print(f'\tquantity of parallel processes: {proc_quan}')
+        with Pool(proc_quan) as pool_obj:
+                exec_time_start = datetime.datetime.now()
+                pool_obj.map(main.revitalize, main.src_file_names)
+                exec_time = datetime.datetime.now() - exec_time_start
+        print(f'\tparallel computation time: {exec_time}')
