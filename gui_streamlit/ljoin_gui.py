df0d2b383ee0bee98d8e2f7a1b95f009cb6c5b69
PlatonB <platon.work@gmail.com>
2022-06-24 00:00:50 +0300

    GUI_S программы пересечения и вычитания, v2.0
    - Виджет с пресетами by_location и by_alleles. by_loc[ation] до этого был отдельным виджетом.

--- gui_streamlit/ljoin_gui.py
+++ gui_streamlit/ljoin_gui.py
@@ -1,4 +1,4 @@
-__version__ = 'v1.1'
+__version__ = 'v2.0'
 
 import streamlit as st
 from descriptions.ljoin_descr import LjoinDescr
@@ -24,10 +24,11 @@ class AddWidgetsRu():
                                                              help='Максимальное количество параллельно обрабатываемых левых коллекций')
                         self.right_coll_names = st.text_input(label='right-coll-names',
                                                               help='Имена правых коллекций (через запятую без пробела; [[все коллекции]]; правая, совпадающая с текущей левой, проигнорируется)')
-                        self.by_loc = st.checkbox(label='by-loc',
-                                                  help='Пересекать или вычитать по геномной локации (экспериментальная фича; src-db-TSV: не применяется)')
+                        self.preset = st.radio(label='preset', options=['', 'by_location', 'by_alleles'],
+                                               help='''Пересекать или вычитать по геномной локации (экспериментальная фича; src-db-TSV: не применяется).
+Пересекать или вычитать ID, уточняя по аллелям совпадения ID (экспериментальная фича; src-db-TSV/BED: не применяется)''')
                         self.lookup_field_path = st.text_input(label='lookup-field-path',
-                                                               help='Точечный путь к полю, по которому пересекать или вычитать (применяется без by-loc; src-db-VCF: [[ID]]; src-db-BED: [[name]], src-db-TSV: [[первое после _id поле]])')
+                                                               help='Точечный путь к полю, по которому пересекать или вычитать (применяется без preset; src-db-VCF: [[ID]]; src-db-BED: [[name]], src-db-TSV: [[первое после _id поле]])')
                         self.action = st.radio(label='action', options=['intersect', 'subtract'],
                                                help='Пересекать или вычитать')
                         self.coverage = st.number_input(label='coverage', value=1, min_value=0, format='%d',
@@ -63,10 +64,11 @@ class AddWidgetsEn():
                                                              help='Maximum quantity of parallel processed left collections')
                         self.right_coll_names = st.text_input(label='right-coll-names',
                                                               help='Right collection names (comma separated without spaces; [[all collections]]; the right one, which matches with current left one, will be ignored)')
-                        self.by_loc = st.checkbox(label='by-loc',
-                                                  help='Intersect or subtract by genomic location (experimental feature; src-db-TSV: not applicable)')
+                        self.preset = st.radio(label='preset', options=['', 'by_location', 'by_alleles'],
+                                               help='''Intersect or subtract by genomic location (experimental feature; src-db-TSV: not applicable).
+Intersect or subtract ID, verifying by alleles ID matches (experimental feature; src-db-TSV/BED: not applicable)''')
                         self.lookup_field_path = st.text_input(label='lookup-field-path',
-                                                               help='Dot path to the field by which to intersect or subtract (applied without by-loc; src-db-VCF: [[ID]]; src-db-BED: [[name]], src-db-TSV: [[first field after _id]])')
+                                                               help='Dot path to the field by which to intersect or subtract (applicable without preset; src-db-VCF: [[ID]]; src-db-BED: [[name]], src-db-TSV: [[first field after _id]])')
                         self.action = st.radio(label='action', options=['intersect', 'subtract'],
                                                help='Intersect or subtract')
                         self.coverage = st.number_input(label='coverage', value=1, min_value=0, format='%d',
@@ -74,7 +76,7 @@ class AddWidgetsEn():
                         self.srt_field_group = st.text_input(label='srt-field-group',
                                                              help='Dot paths to sorted fields (plus separated without spaces; src-db-VCF: [[#CHROM+POS]]; src-db-BED: [[chrom+start+end]]; src-db-VCF, src-db-BED: trg-TSV)')
                         self.srt_order = st.radio(label='srt-order', options=['asc', 'desc'],
-                                                  help='Order of sorting (applied with srt-field-group)')
+                                                  help='Order of sorting (applicable with srt-field-group)')
                         self.proj_field_names = st.text_input(label='proj-field-names',
                                                               help='Selected top level fields (comma separated without spaces; src-db-VCF, src-db-BED: trg-TSV; _id field will not be output)')
                         self.sec_delimiter = st.radio(label='sec-delimiter', options=['colon', 'comma', 'low_line', 'pipe', 'semicolon'], index=1,
