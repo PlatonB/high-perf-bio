83cd668db8b5f84d84cc88ac1bd05e6f9dc2c0bc
PlatonB <platon.work@gmail.com>
2022-08-15 22:28:39 +0300

    CLI программы подсчёта наборов значений, v7.1
    - Обсчитываемое поле по умолчанию для src-db-VCF теперь только ID.

--- cli/count_cli.py
+++ cli/count_cli.py
@@ -1,4 +1,4 @@
-__version__ = 'v7.0'
+__version__ = 'v7.1'
 
 from argparse import ArgumentParser, RawTextHelpFormatter
 from descriptions.count_descr import CountDescr
@@ -23,7 +23,7 @@ def add_args_ru(ver):
                              help='Разрешить перезаписать существующую БД в случае конфликта имён (исходную БД перезаписывать нельзя)')
         opt_grp.add_argument('-f', '--cnt-field-paths', metavar='[None]', dest='cnt_field_paths', type=str,
                              help='''Точечные пути к полям, для которых считать количество каждого набора взаимосвязанных значений (через запятую без пробела;
-src-db-VCF: [[ID,REF,ALT]]; src-db-BED: [[name]]; src-db-TSV: [[первое после _id поле]])''')
+src-db-VCF: [[ID]]; src-db-BED: [[name]]; src-db-TSV: [[первое после _id поле]])''')
         opt_grp.add_argument('-s', '--sec-delimiter', metavar='[low_line]', choices=['colon', 'comma', 'low_line', 'pipe', 'semicolon'], default='low_line', dest='sec_delimiter', type=str,
                              help='{colon, comma, low_line, pipe, semicolon} Знак препинания для восстановления ячейки из списка')
         opt_grp.add_argument('-a', '--quan-thres', metavar='[1]', default=1, dest='quan_thres', type=int,
@@ -57,7 +57,7 @@ def add_args_en(ver):
                              help='Allow overwriting an existing DB in case of names conflict (the source DB cannot be overwritten)')
         opt_grp.add_argument('-f', '--cnt-field-paths', metavar='[None]', dest='cnt_field_paths', type=str,
                              help='''Dot paths to fields for which to count the quantity of each set of related values (comma separated without spaces;
-src-db-VCF: [[ID,REF,ALT]]; src-db-BED: [[name]]; src-db-TSV: [[first field after _id]])''')
+src-db-VCF: [[ID]]; src-db-BED: [[name]]; src-db-TSV: [[first field after _id]])''')
         opt_grp.add_argument('-s', '--sec-delimiter', metavar='[low_line]', choices=['colon', 'comma', 'low_line', 'pipe', 'semicolon'], default='low_line', dest='sec_delimiter', type=str,
                              help='{colon, comma, low_line, pipe, semicolon} Punctuation mark to restore a cell from a list')
         opt_grp.add_argument('-a', '--quan-thres', metavar='[1]', default=1, dest='quan_thres', type=int,
