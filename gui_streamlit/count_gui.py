e4c1f97626b185c653df0c9910f73e86358b435e
PlatonB <platon.work@gmail.com>
2022-11-23 23:50:32 +0300

    GUI_S программы подсчёта наборов значений, v4.0
    - Чекбокс для запрета распаковывать массивы.

--- gui_streamlit/count_gui.py
+++ gui_streamlit/count_gui.py
@@ -1,4 +1,4 @@
-__version__ = 'v3.0'
+__version__ = 'v4.0'
 
 import streamlit as st
 from descriptions.count_descr import CountDescr
@@ -27,6 +27,8 @@ class AddWidgetsRu():
                         self.cnt_field_paths = st.text_input(label='cnt-field-paths',
                                                              help='''Точечные пути к полям, для которых считать количество каждого набора взаимосвязанных значений (через запятую без пробела;
 src-db-VCF: [[ID]]; src-db-BED: [[name]]; src-db-TSV: [[первое после _id поле]])''')
+                        self.not_unfold_arrays = st.checkbox(label='not-unfold-arrays',
+                                                             help='Не разворачивать списки (подсчёт по нескольким полям выдаст ошибку при встрече с массивом)')
                         self.sec_delimiter = st.radio(label='sec-delimiter', options=['colon', 'comma', 'low_line', 'pipe', 'semicolon'], index=2,
                                                       help='Знак препинания для формирования конечных составных значений')
                         self.quan_thres = st.number_input(label='quan-thres', min_value=1, format='%d',
@@ -62,6 +64,8 @@ class AddWidgetsEn():
                         self.cnt_field_paths = st.text_input(label='cnt-field-paths',
                                                              help='''Dot paths to fields for which to count the quantity of each set of related values (comma separated without spaces;
 src-db-VCF: [[ID]]; src-db-BED: [[name]]; src-db-TSV: [[first field after _id]])''')
+                        self.not_unfold_arrays = st.checkbox(label='not-unfold-arrays',
+                                                             help="Don't unfold arrays (counting by multiple fields will cause an error when encountering an array)")
                         self.sec_delimiter = st.radio(label='sec-delimiter', options=['colon', 'comma', 'low_line', 'pipe', 'semicolon'], index=2,
                                                       help='Punctuation mark for forming target compound values')
                         self.quan_thres = st.number_input(label='quan-thres', min_value=1, format='%d',
