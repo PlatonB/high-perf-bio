afe5a9f9f5007c22c46a4abe7888378ce19ebd52
PlatonB <platon.work@gmail.com>
2022-12-27 15:21:22 +0300

    Скрипт запуска GUI (Streamlit), v1.0
    - Стандартизированный приветственный принт.

--- __run_gui_streamlit.sh
+++ __run_gui_streamlit.sh
@@ -1,2 +1,15 @@
-#!/bin/sh
+#!/bin/bash
+
+echo -e "
+A wrapper for command to launch
+Streamlit GUI of high-perf-bio.
+
+Version: v1.0
+Dependencies: -
+Author: Platon Bykadorov (platon.work@gmail.com), 2022
+License: GNU General Public License version 3
+Donate: https://www.tinkoff.ru/rm/bykadorov.platon1/7tX2Y99140/
+Documentation: https://github.com/PlatonB/high-perf-bio/blob/master/README-EN.md
+Bug reports, suggestions, talks: https://github.com/PlatonB/high-perf-bio/issues\n"
+
 streamlit run _gui_streamlit.py
