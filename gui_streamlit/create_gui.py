b6054a5aabadecaf45678d62370cdeb41eeb969c
PlatonB <platon.work@gmail.com>
2023-01-19 23:58:40 +0300

    GUI_S программы создания БД, v3.0
    - Виджет произвольной шапки TSV.
    - Улучшил хэлп виджета количества метастрок.

--- gui_streamlit/create_gui.py
+++ gui_streamlit/create_gui.py
@@ -1,4 +1,4 @@
-__version__ = 'v2.1'
+__version__ = 'v3.0'
 
 import streamlit as st
 from descriptions.create_descr import CreateDescr
@@ -23,7 +23,9 @@ class AddWidgetsRu():
                         self.if_db_exists = st.radio(label='if-db-exists', options=['', 'rewrite', 'replenish'],
                                                      help='Стереть имеющуюся БД или пополнить её данными из новых файлов папки (не допускайте смешивания форматов в одной БД)')
                         self.meta_lines_quan = st.number_input(label='meta-lines-quan', min_value=0, format='%d',
-                                                               help='Количество строк метаинформации (src-VCF: не применяется; src-BED: включите шапку (если есть); src-TSV: не включайте шапку)')
+                                                               help='Количество строк метаинформации (src-VCF: не применяется; src-BED: включите шапку (если есть); src-TSV: не включайте шапку (если есть), либо применяйте arbitrary-header)')
+                        self.arbitrary_header = st.text_input(label='arbitrary-header',
+                                                              help='Произвольная шапка исходных таблиц (через \\t без пробела; src-VCF, src-BED: не применяется)')
                         self.minimal = st.checkbox(label='minimal', value=False,
                                                    help='Загружать только минимально допустимый форматом набор столбцов (src-VCF: 1-ые 8; src-BED: 1-ые 3; src-TSV: не применяется)')
                         self.sec_delimiter = st.radio(label='sec-delimiter', options=['', 'colon', 'comma', 'low_line', 'pipe', 'semicolon'],
@@ -56,7 +58,9 @@ class AddWidgetsEn():
                         self.if_db_exists = st.radio(label='if-db-exists', options=['', 'rewrite', 'replenish'],
                                                      help='Erase the target DB or replenish it with data from new files of the directory (do not allow mixing of formats in the same DB)')
                         self.meta_lines_quan = st.number_input(label='meta-lines-quan', min_value=0, format='%d',
-                                                               help='Quantity of metainformation lines (src-VCF: not applicable; src-BED: include a header (if available); src-TSV: do not include a header)')
+                                                               help='Quantity of metainformation lines (src-VCF: not applicable; src-BED: include a header (if available); src-TSV: do not include a header (if available), otherwise use arbitrary-header)')
+                        self.arbitrary_header = st.text_input(label='arbitrary-header',
+                                                              help='Arbitrary header of source tables (\\t-separated without spaces; src-VCF, src-BED: not applicable)')
                         self.minimal = st.checkbox(label='minimal', value=False,
                                                    help='Upload only the minimum set of columns allowed by the format (src-VCF: 1st 8; src-BED: 1st 3; src-TSV: not applicable)')
                         self.sec_delimiter = st.radio(label='sec-delimiter', options=['', 'colon', 'comma', 'low_line', 'pipe', 'semicolon'],
